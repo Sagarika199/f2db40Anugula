2ffdbfb363dcd4890426366cecd5144b1a536cd0
Sagarika199 <anugulasagarika.09@gmail.com>
2022-11-30 19:18:05 -0600

    Added

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const vehicleSchema = mongoose.Schema({ 
-VehicleName: String, 
-VehicleType: String, 
-VehicleCost: Number 
+VehicleName: {type:String,minlength:3}, 
+VehicleType: {type:String}, 
+VehicleCost:{type:Number,min:20000,max:1000000}  
 }) 
  
 module.exports = mongoose.model("vehicle", 
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -1,17 +1,26 @@
 var express = require('express');
 const vehicle_controlers= require('../controllers/vehicle');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
 
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
 /* GET costumes */ 
 router.get('/', vehicle_controlers.vehicle_view_all_Page ); 
 
-/* GET detail cereal page */
+/* GET detail vehicle page */
 router.get('/detail',vehicle_controlers.vehicle_view_one_Page);
-/* GET create cereal page */
+/* GET create vehicle page */
 router.get('/create', vehicle_controlers.vehicle_create_Page);
 /* GET create update page */
-router.get('/update', vehicle_controlers.vehicle_update_Page);
-/* GET delete cereal page */
+router.get('/update',secured, vehicle_controlers.vehicle_update_Page);
+/* GET delete vehicle page */
 router.get('/delete', vehicle_controlers.vehicle_delete_Page);
 
 module.exports = router; 
